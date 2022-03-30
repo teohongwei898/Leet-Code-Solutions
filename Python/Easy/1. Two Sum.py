30e5a768285b8f1eee6043763b2bc38fd4e49d4e
Hong Wei <teohongwei898@gmail.com>
2022-03-30 17:45:53 +0800

    Create 1. Two Sum.py

---
+++ Python/Easy/1. Two Sum.py
@@ -0,0 +1,24 @@
+#Brute Force
+
+class Solution:
+    def BruteForcetwoSum(self, nums: List[int], target: int) -> List[int]:        
+        for i in range(len(nums)):    
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+              
+#Time: O(n), Size: O(n)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      #Initialize Hashmap
+      #Check if Target - value is in hashmap, if not, add index to hashmap
+      #In the first iteration, hashmap is empty. Hence 100% will map to hashmap first.
+      #End result, may not even need to map everything to hashmap since the results may be in the middle of the hashmap.
+      #enumerate tracks index and value in array.
+      prevMap = {} #Value:Index
+      for i,n in enmuerate(nums):
+        diff = target - n
+        if diff in prevMap:
+          return (prevMap[diff],i)
+        prevMap[n] = i
+                     
+              
