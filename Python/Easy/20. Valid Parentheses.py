dc4cfa2c7dd273178399caca12ed9efee8b46fb2
Hong Wei <teohongwei898@gmail.com>
2022-03-31 11:53:59 +0800

    Update 20. Valid Parentheses.py

--- Python/Easy/20. Valid Parentheses.py
+++ Python/Easy/20. Valid Parentheses.py
@@ -7,7 +7,7 @@ class Solution:
             if s[i] == ("(" or "{" or "["):
                 stack.append(s[i])
             if s[i] == ")":
-                if not stack or stack.pop() != "(":
+                if not stack or stack.pop() != "(":                 #If stack empty or the one that is being popped off does not match, return False
                     return False
                 
             if s[i] == "]":
