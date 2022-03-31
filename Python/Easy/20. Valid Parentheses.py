97877d4dedde585423dd5c1b7f6695cabdd6bd62
Hong Wei <teohongwei898@gmail.com>
2022-03-31 12:57:26 +0800

    Update 20. Valid Parentheses.py

--- Python/Easy/20. Valid Parentheses.py
+++ Python/Easy/20. Valid Parentheses.py
@@ -3,24 +3,27 @@ class Solution:
         stack = []
         if len(s)<2:
             return False
-        for i in range(len(s)-1):
-            if s[i] == ("(" or "{" or "["):
-                stack.append(s[i])
-            if s[i] == ")":
-                if not stack or stack.pop() != "(":                 #If stack empty or the one that is being popped off does not match, return False
+        for ch in s:
+            if ch == "(" or ch == "{" or ch == "[":
+                stack.append(ch)
+            elif ch == ")":
+                if not stack or stack.pop() != "(":
                     return False
-                
-            if s[i] == "]":
+            elif ch == "]":
                 if not stack or stack.pop() != "[":
-                    return False
-                
-            if s[i] == "}":
-                if not stack == True or stack.pop() != "{":
-                    return False
-                
-        
-        return True
+                    return False                
+            elif ch == "}":
+                if not stack or stack.pop() != "{":
+                    return False                
+        if not stack:
+            return True
+        else:
+            return False
             
             
-                
+#First check if length is 1/0, if so, false since no perfect scenario for that
+#If opening brackets, add into stack.
+#If closing brackets, check if stack is empty.
+#If stack empty, means no perfect = False. If got item but popping off a different symbol also wrong
+#End of all, need to check that stack is empty. To take care of scenarios like "(((". Since they are all appended!
         
