1d9d0287b31d32225c3ef613baf32929d846b399
Hong Wei <teohongwei898@gmail.com>
2022-04-13 13:13:52 +0800

    Update 167. Two Sum II - Input Array Is Sorted.py

--- Python/Two Pointers/167. Two Sum II - Input Array Is Sorted.py
+++ Python/Two Pointers/167. Two Sum II - Input Array Is Sorted.py
@@ -6,3 +6,18 @@ class Solution:
             if diff in hashMap:
                 return [hashMap[diff]+1,i+1]
             hashMap[n] = i
+            
+            
+            
+           #or
+        
+ class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        L, R =0, len(numbers)-1
+        while L<R:
+            if numbers[L]+numbers[R] > target:
+                R-=1
+            elif numbers[L]+numbers[R] < target:
+                L+=1
+            else:
+                return [L+1, R+1]
