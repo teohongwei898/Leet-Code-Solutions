2e5261adc37489f12e4d8610631323aa255bbd0d
Hong Wei <teohongwei898@gmail.com>
2022-04-13 10:27:07 +0800

    Update 36. Valid Sudoku.py

--- Python/Arrays & Hashing/36. Valid Sudoku.py
+++ Python/Arrays & Hashing/36. Valid Sudoku.py
@@ -1,35 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        return (self.is_row_valid(board) and self.is_col_valid(board) and self.is_square_valid(board))
-    
-    def is_row_valid(self,row, board):
-        hashrowset = set()
-        for i in range(0,8):
-            if board[row][i] in hashrowset:
-                return False
-            if board[row][i] != "."
-                hashrowset.add(board[row][i])
-         return True
-      
-   def is_col_valid(self,col, board):
-        hashcolset = set()
-        for i in range(0,8):
-            if board[i][col] in hashrowset:
-                return False
-            if board[row][i] != "."
-                hashrowset.add(board[i][col])
-         return True
-      
-    def is_square_valid(self,col, board):
-        hashsqset = set()
-        for i in range(a,3):
-          for j in range(b,3):
-              curr = board[i][j]
-            if board[i][j] in hashrowset:
-                return False
-         return True  
-   
-  
-  
-
-            
+        rows = defaultdict(set)
+        col = defaultdict(set)
+        squares = defaultdict(set)
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if board[r][c] in rows[r] or  board[r][c] in col[c] or board[r][c] in squares[r//3, c//3]:
+                    return False
+                else:
+                    rows[r].add(board[r][c])
+                    col[c].add(board[r][c])
+                    squares[r//3, c//3].add(board[r][c])
+        return True
+                    
+                    
